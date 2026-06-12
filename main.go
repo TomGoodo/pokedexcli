9ba0435924a8d3d32ad9dcae93ef12cded579849
Tom Goodison <tomgoodo@protonmail.com>
2026-06-12 18:05:48 +1000

    feat: added command structure and help/exit commands

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			res := scanner.Text()
-			words := cleanInput(res)
-			fmt.Printf("Your command was: %s\n", words[0])
-		}
-	}
+	startRepl()	
 }
--- repl.go
+++ repl.go
@@ -1,10 +1,53 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+type cliCommands struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func getCommands() map[string]cliCommands {
+
+	return map[string]cliCommands{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		cmd, ok := commands[words[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		cmd.callback()
+	}
 }
