09a539c893550f2a56a0dbdc3baec8833a41b488
Tom Goodison <tomgoodo@protonmail.com>
2026-06-12 17:14:13 +1000

    feat: added repl loop and input

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
-
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			res := scanner.Text()
+			words := cleanInput(res)
+			fmt.Printf("Your command was: %s\n", words[0])
+		}
+	}
 }
