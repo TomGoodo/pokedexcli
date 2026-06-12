62d5f9ca683d157089394fc92a7640a5f1286df7
Tom Goodison <tomgoodo@protonmail.com>
2026-06-12 16:43:42 +1000

    feat: added REPL clean input

---
+++ repl_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input	string
+		expected	[]string
+	}{
+		{
+			input:	"  hello world ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: " hello ",
+			expected: []string{"hello"},
+		},
+		{
+			input: "  ",
+			expected: []string{},
+		},
+		{
+			input: " hello            world",
+			expected: []string{"hello","world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths dont match: '%v' vs '%v'", actual, c.expected)
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("word %v wrong, got %v, want %v", c.input, actual, c.expected)
+			}
+		}
+	}
+}
